493a3562605dc524a6f6cd79f846c3e3e1a4a3f1
aatar <ari.atar1997@gmail.com>
2019-08-05 14:04:23 -0300

    checking repeated email

--- node-sequelize/controllers/user.js
+++ node-sequelize/controllers/user.js
@@ -1,15 +1,26 @@
 const User = require('../models').User;
 
+const add = (req, res) => {
+    return User.findAll({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then((user) => {
+        if(user.length > 0) res.status(400).send("Email is already in use");
+        return User
+        .create({
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then((user) => res.status(201).send(user))
+        .catch((error) => res.status(400).send(error));
+    })
+    .catch((error) => res.status(400).send(error));
+};
+
 module.exports = {
-  add(req, res) {
-    return User
-      .create({
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: req.body.password,
-      })
-      .then((user) => res.status(201).send(user))
-      .catch((error) => res.status(400).send(error));
-  }
+    add
 };
