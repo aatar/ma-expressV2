1bba484d6116cb40a8f7cae9accb76420ab9fa30
aatar <ari.atar1997@gmail.com>
2019-08-05 12:05:28 -0300

    generated project

--- nodejsTraining/app/errors.js
+++
@@ -1,7 +0,0 @@
-const internalError = (message, internalCode) => ({
-  message,
-  internalCode
-});
-
-exports.DEFAULT_ERROR = 'default_error';
-exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
