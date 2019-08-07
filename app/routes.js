b34e164c5946ac9c23ddb23c3c0b675e3871166b
aatar <ari.atar1997@gmail.com>
2019-08-07 17:02:54 -0300

    change

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,12 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
-const { listAlbums } = require('./controllers/album');
+const { listAlbums, listPhotos } = require('./controllers/album');
 
 exports.init = app => {
   app.get('/', (req, res) => res.status(200).send('Welcome!!'));
   app.get('/health', healthCheck);
   app.get('/albums', listAlbums);
-  app.get('/albums/:id/photos', listAlbums);
+  app.get('/albums/:id/photos', listPhotos);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
