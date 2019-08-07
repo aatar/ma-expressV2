17081a17a32ed556ec349c7e394eba3978a9115a
aatar <ari.atar1997@gmail.com>
2019-08-07 17:36:39 -0300

    signup finished

---
+++ app/controllers/user.js
@@ -0,0 +1,34 @@
+const { User } = require('../models');
+const { validateEmail, validatePassword } = require('../helpers/utils');
+const md5 = require('crypto-js').MD5;
+
+const add = (req, res) => {
+  if (!validateEmail(req.body.email)) {
+    res.status(400).send('Email is not valid');
+  }
+  if (!validatePassword(req.body.password)) {
+    res.status(400).send('Password must be alphanumeric and have at least 8 characters');
+  }
+
+  return User.findAll({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (user.length > 0) {
+        res.status(400).send('Email is already in use');
+      }
+      return User.create({
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: md5(req.body.password).toString()
+      })
+        .then(userCreated => res.status(201).send(userCreated))
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+};
+
+module.exports = { add };
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,11 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const { listAlbums, listPhotos } = require('./controllers/album');
+const { add: addUser } = require('./controllers/user');
 
 exports.init = app => {
   app.get('/', (req, res) => res.send('Welcome to Heroku'));
   app.get('/health', healthCheck);
   app.get('/albums', listAlbums);
   app.get('/albums/:id/photos', listPhotos);
+  app.post('/users', addUser);
 };
