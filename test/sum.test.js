74219529eb0f70ff1492172859a04222a8a54b83
aatar <ari.atar1997@gmail.com>
2019-08-08 12:26:45 -0300

    going on

--- test/sum.test.js
+++ test/sum.test.js
@@ -4,12 +4,12 @@ test('should insert user', () => {
   const body = {
     name: 'Ariel',
     surname: 'Atar',
-    email: 'marcos@wolox.com.ar',
+    email: 'marcos4@wolox.com.ar',
     password: '123123123'
   };
   fetch('http://localhost:8080/users', {
     method: 'post',
     body: JSON.stringify(body),
     headers: { 'Content-Type': 'application/json' }
-  }).then(res => expect(res).toBe('OK'));
+  }).then(response => expect(response.status).toBe(201));
 });
