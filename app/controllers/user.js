b2104b48b9db58b2958154d7407331beba036bd5
aatar <ari.atar1997@gmail.com>
2019-08-12 12:40:00 -0300

    fixed error

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { User } = require('../models'),
   { signupError } = require('../errors'),
   { signUpMapper } = require('../mappers/user'),
@@ -8,24 +7,6 @@ const { User } = require('../models'),
 exports.addUser = (req, res, next) => {
   logger.info('Searching user...');
   return User.findOne({
-=======
-const { User } = require('../models');
-const { validateEmail, validatePassword, validateArguments } = require('../helpers/utils');
-const md5 = require('crypto-js').MD5;
-
-const add = (req, res) => {
-  if (!validateArguments(req.body)) {
-    res.status(400).send('Argument/s missing');
-  }
-  if (!validateEmail(req.body.email)) {
-    res.status(400).send('Email is not valid');
-  }
-  if (!validatePassword(req.body.password)) {
-    res.status(400).send('Password must be alphanumeric and have at least 8 characters');
-  }
-
-  return User.findAll({
->>>>>>> finished
     where: {
       email: req.body.email
     }
@@ -42,15 +23,3 @@ const add = (req, res) => {
     })
     .catch(next);
 };
-<<<<<<< HEAD
-=======
-
-const deleteAll = (req, res) =>
-  User.destroy({
-    truncate: true
-  })
-    .then(() => res.status(200).send('Deleted All'))
-    .catch(error => res.status(400).send(error));
-
-module.exports = { add, deleteAll };
->>>>>>> finished
