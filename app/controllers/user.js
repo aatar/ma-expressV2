0a1860f901809d661548f1cc9635317d6d824658
aatar <ari.atar1997@gmail.com>
2019-08-12 14:06:53 -0300

    changed pass encryption to bcrypt

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,5 @@
 const { User } = require('../models');
-const md5 = require('crypto-js').MD5;
+const bcrypt = require('bcryptjs');
 
 const add = (req, res) =>
   User.findAll({
@@ -11,14 +11,16 @@ const add = (req, res) =>
       if (user.length > 0) {
         res.status(400).send('Email is already in use');
       }
-      return User.create({
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: md5(req.body.password).toString()
-      })
-        .then(() => res.status(201).send('OK'))
-        .catch(error => res.status(400).send(error));
+      bcrypt.hash(req.body.password, 10).then(hashedPassword =>
+        User.create({
+          name: req.body.name,
+          surname: req.body.surname,
+          email: req.body.email,
+          password: hashedPassword
+        })
+          .then(() => res.status(201).send('OK'))
+          .catch(error => res.status(400).send(error))
+      );
     })
     .catch(error => res.status(400).send(error));
 
