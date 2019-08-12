1fcfaa183bfa8ef1fc39a1bfb25574644e7128de
aatar <ari.atar1997@gmail.com>
2019-08-12 14:54:34 -0300

    working properly

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,7 +2,8 @@ const { User } = require('../models'),
   { signupError } = require('../errors'),
   { signUpMapper } = require('../mappers/user'),
   { serializeUser } = require('../serializers/user'),
-  logger = require('../logger');
+  logger = require('../logger'),
+  jwt = require('jsonwebtoken');
 
 exports.addUser = (req, res, next) => {
   logger.info('Searching user...');
@@ -22,4 +23,32 @@ exports.addUser = (req, res, next) => {
       });
     })
     .catch(next);
+};
+
+exports.login = (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (user) {
+        bcrypt.compare(req.body.password, user[0].password, (err, areEquals) => {
+          if (areEquals) {
+            const token = jwt.sign({ email: req.body.email }, process.env.PRIVATE_KEY, {
+              algorithm: 'HS256'
+            });
+            res
+              .status(200)
+              .set('Authorization', `Bearer ${token}`)
+              .send('OK');
+          } else {
+            throw signupError('Email or password is incorrect');
+          }
+        });
+      } else {
+        throw signupError('Email or password is incorrect');
+      }
+    })
+    .catch(error => res.status(400).send(error));
 };
