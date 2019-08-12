1cf6c22ecc98f3b69137d6d809a4901a15cd3cc3
aatar <ari.atar1997@gmail.com>
2019-08-12 12:40:00 -0300

    fixed error

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-negated-condition */
 const { User } = require('../models');
 const { validateEmail, validatePassword, validateArguments } = require('../helpers/utils');
 const md5 = require('crypto-js').MD5;
@@ -5,33 +6,32 @@ const md5 = require('crypto-js').MD5;
 const add = (req, res) => {
   if (!validateArguments(req.body)) {
     res.status(400).send('Argument/s missing');
-  }
-  if (!validateEmail(req.body.email)) {
+  } else if (!validateEmail(req.body.email)) {
     res.status(400).send('Email is not valid');
-  }
-  if (!validatePassword(req.body.password)) {
+  } else if (!validatePassword(req.body.password)) {
     res.status(400).send('Password must be alphanumeric and have at least 8 characters');
-  }
-
-  return User.findAll({
-    where: {
-      email: req.body.email
-    }
-  })
-    .then(user => {
-      if (user.length > 0) {
-        res.status(400).send('Email is already in use');
+  } else {
+    return User.findAll({
+      where: {
+        email: req.body.email
       }
-      return User.create({
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: md5(req.body.password).toString()
-      })
-        .then(() => res.status(201).send('OK'))
-        .catch(error => res.status(400).send(error));
     })
-    .catch(error => res.status(400).send(error));
+      .then(user => {
+        if (user.length > 0) {
+          res.status(400).send('Email is already in use');
+        }
+        return User.create({
+          name: req.body.name,
+          surname: req.body.surname,
+          email: req.body.email,
+          password: md5(req.body.password).toString()
+        })
+          .then(() => res.status(201).send('OK'))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  }
+  return '';
 };
 
 const deleteAll = (req, res) =>
