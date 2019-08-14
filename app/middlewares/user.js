1be8a4349ce63d5aec22cfa962218c01b3f87436
aatar <ari.atar1997@gmail.com>
2019-08-14 16:12:05 -0300

    finished

--- app/middlewares/user.js
+++ app/middlewares/user.js
@@ -1,5 +1,14 @@
-const { checkIfUserIsLogged: userCheck } = require('./common');
+const { checkIfUserIsLogged: userCheck } = require('./common'),
+  { User } = require('../models');
 
 exports.checkIfUserIsLogged = (req, res, next) => userCheck(req, res, next, false);
 
-exports.checkIfUserIsAdmin = (req, res, next) => userCheck(req, res, next, true);
+exports.checkIfUserIsAdmin = (req, res, next) => {
+  User.findAll().then(users => {
+    if (users.length > 0) {
+      userCheck(req, res, next, true);
+    } else {
+      next();
+    }
+  });
+};
