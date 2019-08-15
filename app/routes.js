b9eabff439853f14a42a31711a9a05a0b23d6509
aatar <ari.atar1997@gmail.com>
2019-08-15 12:00:09 -0300

    change for heroku

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@ const { healthCheck } = require('./controllers/healthCheck');
 const { listAlbums, listPhotos } = require('./controllers/album');
 
 exports.init = app => {
+  app.get('/', (req, res) => res.send('Welcome to Heroku'));
   app.get('/health', healthCheck);
   app.get('/albums', listAlbums);
   app.get('/albums/:id/photos', listPhotos);
