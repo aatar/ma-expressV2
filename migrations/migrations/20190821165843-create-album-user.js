7afbafe460976a2ab4d73b6925e03a5355010949
aatar <ari.atar1997@gmail.com>
2019-08-21 14:07:30 -0300

    created new model

--- migrations/migrations/20190821165843-create-album-user.js
+++ migrations/migrations/20190821165843-create-album-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('AlbumUsers', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('AlbumUsers', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,9 +25,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('AlbumUsers');
-  }
+    }),
+  down: queryInterface => queryInterface.dropTable('AlbumUsers')
 };
