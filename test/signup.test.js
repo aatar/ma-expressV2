37799ebca7b012838eafc143337992d471c470e7
aatar <ari.atar1997@gmail.com>
2019-08-28 15:42:59 -0300

    tests finished

--- test/signup.test.js
+++ test/signup.test.js
@@ -1,6 +1,68 @@
-const fetch = require('node-fetch');
-const { shouldNotInsertUser } = require('./helper');
+// const fetch = require('node-fetch');
+// const { shouldNotInsertUser } = require('./helper');
+const request = require('supertest');
+const app = require('../app');
+const { defaultUser } = require('./constants');
 
+describe('POST /users', () => {
+  beforeAll(() => {
+    request(app).delete('/users');
+  });
+
+  /* test('It should delete users', async () => {
+    const response = await request(app).delete('/users');
+    expect(response.statusCode).toBe(200);
+  });*/
+
+  test('should insert user', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send(defaultUser)
+      .set('Accept', 'application/json');
+    expect(response.statusCode).toBe(201);
+  });
+
+  test('already used email', async () => {
+    await request(app)
+      .post('/users')
+      .send(defaultUser)
+      .set('Accept', 'application/json');
+    const response = await request(app)
+      .post('/users')
+      .send(defaultUser)
+      .set('Accept', 'application/json');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('password is not valid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({ ...defaultUser, password: '234234' })
+      .set('Accept', 'application/json');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('email is not valid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({ ...defaultUser, email: 'marcos.atar@woleox.com.ar' })
+      .set('Accept', 'application/json');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('arguments missing', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: defaultUser.name,
+        email: defaultUser.email,
+        password: defaultUser.password
+      })
+      .set('Accept', 'application/json');
+    expect(response.statusCode).toBe(400);
+  });
+});
+/*
 test('should delete users', () => {
   fetch('http://localhost:8080/users', {
     method: 'delete'
@@ -58,3 +120,4 @@ test('email is not valid', () => {
   };
   shouldNotInsertUser(body);
 });
+*/
