d320e778bb87242973e7cf11b4055da5097c8396
Ariel <ari.atar1997@gmail.com>
2019-09-18 15:22:43 -0300

    added loggers

--- app/middlewares/album.js
+++ app/middlewares/album.js
@@ -1,8 +1,10 @@
 const { albumUser } = require('../models'),
-  { alreadyBoughtAlbumError, didntBuyAlbumError } = require('../errors');
+  { alreadyBoughtAlbumError, didntBuyAlbumError } = require('../errors'),
+  logger = require('../logger');
 
-const userBoughtAlbum = (req, res, next) =>
-  albumUser
+const userBoughtAlbum = (req, res, next) => {
+  logger.info('Searching album...');
+  return albumUser
     .findOne({
       where: {
         albumId: req.params.id,
@@ -11,9 +13,11 @@ const userBoughtAlbum = (req, res, next) =>
     })
     .then(album => album)
     .catch(next);
+};
 
-exports.checkIfUserDidntBuyAlbum = (req, res, next) =>
-  userBoughtAlbum(req, res, next)
+exports.checkIfUserDidntBuyAlbum = (req, res, next) => {
+  logger.info('Checking if did not buy album...');
+  return userBoughtAlbum(req, res, next)
     .then(response => {
       if (response) {
         throw alreadyBoughtAlbumError('You have already bought that album');
@@ -22,9 +26,11 @@ exports.checkIfUserDidntBuyAlbum = (req, res, next) =>
       }
     })
     .catch(next);
+};
 
-exports.checkIfUserBoughtAlbum = (req, res, next) =>
-  userBoughtAlbum(req, res, next)
+exports.checkIfUserBoughtAlbum = (req, res, next) => {
+  logger.info('Checking if bought album...');
+  return userBoughtAlbum(req, res, next)
     .then(response => {
       if (response) {
         return next();
@@ -32,3 +38,4 @@ exports.checkIfUserBoughtAlbum = (req, res, next) =>
       throw didntBuyAlbumError('You did not buy that album');
     })
     .catch(next);
+};
