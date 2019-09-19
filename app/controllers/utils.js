9a6553a27373d7b16719478fe2182e2e3c196932
Ariel <ari.atar1997@gmail.com>
2019-09-19 15:44:07 -0300

    change requested

--- app/controllers/utils.js
+++ app/controllers/utils.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 const logger = require('../logger');
 const bcrypt = require('bcryptjs');
+const config = require('../../config');
 
 exports.signJWT = payload => {
   logger.info('Signing JWT...');
   return jwt.sign(payload, process.env.PRIVATE_KEY, {
     algorithm: 'HS256',
-    expiresIn: process.env.TOKEN_EXPIRATION_TIME
+    expiresIn: config.common.tokenExpirationTime
   });
 };
 
