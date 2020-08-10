a2f097217d28755adc3650fb77bb41546be5733b
Ariel <ari.atar1997@gmail.com>
2020-08-09 23:50:35 -0300

    added infobae and clarin

--- app/controllers/googleNews.js
+++ app/controllers/googleNews.js
@@ -3,10 +3,43 @@ const { getNews } = require('../services/googleNews'),
   schedule = require('node-schedule'),
   { GoogleNews } = require('../models');
 
+exports.getInfobaeNews = (req, res, next) => {
+  logger.info('Getting Infobae news...');
+  return getNews('https://www.infobae.com/feeds/rss/')
+    .then(response => {
+      let ans = [];
+      // eslint-disable-next-line array-callback-return
+      response.items.map(item => {
+        const { title, link, pubDate, content } = item;
+        ans = [...ans, { title, link, pubDate, content }];
+      });
+      return res.send(ans);
+    })
+    .catch(next);
+};
+
+exports.getClarinNews = (req, res, next) => {
+  logger.info('Getting Clarin news...');
+  return getNews('https://www.clarin.com/rss/politica/')
+    .then(response => {
+      let ans = [];
+      // eslint-disable-next-line array-callback-return
+      response.items.map(item => {
+        const { title, link, pubDate, content } = item;
+        ans = [...ans, { title, link, pubDate, content }];
+      });
+      return res.send(ans);
+    })
+    .catch(next);
+};
+
 exports.getPeriodicGoogleNews = (req, res, next) => {
   schedule.scheduleJob('0 * * * *', () => {
     logger.info('Getting Google news...');
-    return getNews()
+    return getNews(
+      // eslint-disable-next-line max-len
+      'https://news.google.com/rss/topics/CAAqKAgKIiJDQkFTRXdvSkwyMHZNREZqY0hsNUVnWmxjeTAwTVRrb0FBUAE?hl=es-419&gl=AR&ceid=AR%3Aes-419'
+    )
       .then(response => {
         response.items.map(async item => {
           const news = await GoogleNews.findOne({
