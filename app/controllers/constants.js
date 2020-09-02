396444e0ad2ec253890d0e29a7aa5e2bc29023f9
Ariel <ari.atar1997@gmail.com>
2020-09-01 21:03:20 -0300

    added ALL SOURCES

--- app/controllers/constants.js
+++ app/controllers/constants.js
@@ -1,17 +1,7 @@
+/* eslint-disable max-lines */
 exports.SOURCES = [
   {
     id: 0,
-    name: 'Infobae',
-    feeds: [
-      {
-        id: 0,
-        title: 'Infobae',
-        link: 'https://www.infobae.com/feeds/rss/'
-      }
-    ]
-  },
-  {
-    id: 1,
     name: 'Clarin',
     feeds: [
       {
@@ -213,6 +203,523 @@ exports.SOURCES = [
         id: 39,
         title: 'ARQ',
         link: 'https://www.clarin.com/rss/arq/'
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: 'Infobae',
+    feeds: [
+      {
+        id: 0,
+        title: 'Infobae',
+        link: 'https://www.infobae.com/feeds/rss/'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'La Nacion',
+    feeds: [
+      {
+        id: 0,
+        title: 'Ultimas Noticias',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/origen=2'
+      },
+      {
+        id: 1,
+        title: 'Titulos Ed. Impresa',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/origen=1'
+      },
+      {
+        id: 2,
+        title: 'Politica',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=30'
+      },
+      {
+        id: 3,
+        title: 'Economia',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=272'
+      },
+      {
+        id: 4,
+        title: 'Deportes',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=131'
+      },
+      {
+        id: 5,
+        title: 'Sociedad',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7773'
+      },
+      {
+        id: 6,
+        title: 'Opinion',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=28'
+      },
+      {
+        id: 7,
+        title: 'Espectaculos',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=120'
+      },
+      {
+        id: 8,
+        title: 'El Mundo',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7'
+      },
+      {
+        id: 9,
+        title: 'Revista LA NACION',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=494'
+      },
+      {
+        id: 10,
+        title: 'Suplemento Enfoques',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=421'
+      },
+      {
+        id: 11,
+        title: 'Suplemento Comercio Exterior',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=347'
+      },
+      {
+        id: 12,
+        title: 'Suplemento Tecnologia',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=432'
+      },
+      {
+        id: 13,
+        title: 'Suplemento Turismo',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=504'
+      },
+      {
+        id: 14,
+        title: 'Suplemento Al Volante',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=371'
+      },
+      {
+        id: 15,
+        title: 'Suplemento El Campo',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=337'
+      },
+      {
+        id: 16,
+        title: 'Suplemento Moda y Belleza',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1312'
+      },
+      {
+        id: 17,
+        title: 'Suplemento Inmuebles Comerciales',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1363'
+      },
+      {
+        id: 18,
+        title: 'Suplemento Countries',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1348'
+      },
+      {
+        id: 19,
+        title: 'Suplemento Comunidad',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1344'
+      },
+      {
+        id: 20,
+        title: 'adnCultura',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=6734'
+      },
+      {
+        id: 21,
+        title: 'The Wall Street Journal Americas',
+        link: 'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=6373'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Pagina12',
+    feeds: [
+      {
+        id: 0,
+        title: 'Portada',
+        link: 'https://www.pagina12.com.ar/rss/portada'
+      },
+      {
+        id: 1,
+        title: 'Edicion',
+        link: 'https://www.pagina12.com.ar/rss/edicion-impresa'
+      },
+      {
+        id: 2,
+        title: 'Espectaculos',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/cultura-y-espectaculos/notas'
+      },
+      {
+        id: 3,
+        title: 'Rosario 12',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/rosario12/notas'
+      },
+      {
+        id: 4,
+        title: 'El Pais',
+        link: 'https://www.pagina12.com.ar/rss/secciones/el-pais/notas'
+      },
+      {
+        id: 5,
+        title: 'Economia',
+        link: 'https://www.pagina12.com.ar/rss/secciones/economia/notas'
+      },
+      {
+        id: 6,
+        title: 'Sociedad',
+        link: 'https://www.pagina12.com.ar/rss/secciones/sociedad/notas'
+      },
+      {
+        id: 7,
+        title: 'El Mundo',
+        link: 'https://www.pagina12.com.ar/rss/secciones/el-mundo/notas'
+      },
+      {
+        id: 8,
+        title: 'Deportes',
+        link: 'https://www.pagina12.com.ar/rss/secciones/deportes/notas'
+      },
+      {
+        id: 9,
+        title: 'Cultura',
+        link: 'https://www.pagina12.com.ar/rss/secciones/cultura/notas'
+      },
+      {
+        id: 10,
+        title: 'Universidad',
+        link: 'https://www.pagina12.com.ar/rss/secciones/universidad/notas'
+      },
+      {
+        id: 11,
+        title: 'Ciencia',
+        link: 'https://www.pagina12.com.ar/rss/secciones/ciencia/notas'
+      },
+      {
+        id: 12,
+        title: 'Psicologia',
+        link: 'https://www.pagina12.com.ar/rss/secciones/psicologia/notas'
+      },
+      {
+        id: 13,
+        title: 'Ajedrez',
+        link: 'https://www.pagina12.com.ar/rss/secciones/ajedrez/notas'
+      },
+      {
+        id: 14,
+        title: 'La Ventana',
+        link: 'https://www.pagina12.com.ar/rss/secciones/la-ventana/notas'
+      },
+      {
+        id: 15,
+        title: 'Dialogos',
+        link: 'https://www.pagina12.com.ar/rss/secciones/dialogos/notas'
+      },
+      {
+        id: 16,
+        title: 'Hoy',
+        link: 'https://www.pagina12.com.ar/rss/secciones/hoy/notas'
+      },
+      {
+        id: 17,
+        title: 'Plastica',
+        link: 'https://www.pagina12.com.ar/rss/secciones/plastica/notas'
+      },
+      {
+        id: 18,
+        title: 'Cartas de Lectores',
+        link: 'https://www.pagina12.com.ar/rss/secciones/cartas-de-lectores/notas'
+      },
+      {
+        id: 19,
+        title: 'Cash',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/cash/notas'
+      },
+      {
+        id: 20,
+        title: 'Las 12',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/las12/notas'
+      },
+      {
+        id: 21,
+        title: 'Libero',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/libero/notas'
+      },
+      {
+        id: 22,
+        title: 'M2',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/m2/notas'
+      },
+      {
+        id: 23,
+        title: 'NO',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/no/notas'
+      },
+      {
+        id: 24,
+        title: 'Radar',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/radar/notas'
+      },
+      {
+        id: 25,
+        title: 'Radar Libros',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/radar-libros/notas'
+      },
+      {
+        id: 26,
+        title: 'Satira 12',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/satira12/notas'
+      },
+      {
+        id: 27,
+        title: 'Soy',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/soy/notas'
+      },
+      {
+        id: 28,
+        title: 'Turismo',
+        link: 'https://www.pagina12.com.ar/rss/suplementos/turismo/notas'
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Ambito',
+    feeds: [
+      {
+        id: 0,
+        title: 'Portada',
+        link: 'https://www.ambito.com/rss/home.xml'
+      },
+      {
+        id: 1,
+        title: 'Ultimas noticias',
+        link: 'https://www.ambito.com/rss/ultimas-noticias.xml'
+      },
+      {
+        id: 2,
+        title: 'Economia',
+        link: 'https://www.ambito.com/rss/economia.xml'
+      },
+      {
+        id: 3,
+        title: 'Finanzas',
+        link: 'https://www.ambito.com/rss/finanzas.xml'
+      },
+      {
+        id: 4,
+        title: 'Politica',
+        link: 'https://www.ambito.com/rss/politica.xml'
+      },
+      {
+        id: 5,
+        title: 'Negocios',
+        link: 'https://www.ambito.com/rss/negocios.xml'
+      },
+      {
+        id: 6,
+        title: 'Opinion',
+        link: 'https://www.ambito.com/rss/opinion.xml'
+      },
+      {
+        id: 7,
+        title: 'Lifestyle',
+        link: 'https://www.ambito.com/rss/lifestyle.xml'
+      },
+      {
+        id: 8,
+        title: 'Nacional',
+        link: 'https://www.ambito.com/rss/nacional.xml'
+      },
+      {
+        id: 9,
+        title: 'Edicion Impresa',
+        link: 'https://www.ambito.com/rss/edicion-impresa.xml'
+      },
+      {
+        id: 10,
+        title: 'Mundo',
+        link: 'https://www.ambito.com/rss/mundo.xml'
+      },
+      {
+        id: 11,
+        title: 'Informacion General',
+        link: 'https://www.ambito.com/rss/informacion-general.xml'
+      },
+      {
+        id: 12,
+        title: 'Autos',
+        link: 'https://www.ambito.com/rss/autos.xml'
+      },
+      {
+        id: 13,
+        title: 'Estilo A',
+        link: 'https://www.ambito.com/rss/estilo-a.xml'
+      },
+      {
+        id: 14,
+        title: 'Municipios',
+        link: 'https://www.ambito.com/rss/municipios.xml'
+      },
+      {
+        id: 15,
+        title: 'Espectaculos',
+        link: 'https://www.ambito.com/rss/espectaculos.xml'
+      },
+      {
+        id: 16,
+        title: 'Ambito Biz',
+        link: 'https://www.ambito.com/rss/ambito-biz.xml'
+      },
+      {
+        id: 17,
+        title: 'Novedades Fiscales',
+        link: 'https://www.ambito.com/rss/novedades-fiscales.xml'
+      },
+      {
+        id: 18,
+        title: 'Deportes',
+        link: 'https://www.ambito.com/rss/deportes.xml'
+      },
+      {
+        id: 19,
+        title: 'Copa America',
+        link: 'https://www.ambito.com/rss/copa-america.xml'
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'TN',
+    feeds: [
+      {
+        id: 0,
+        title: 'Canal TN Hoy',
+        link: 'https://tn.com.ar/rss.xml'
+      },
+      {
+        id: 1,
+        title: 'Canal Politica',
+        link: 'https://tn.com.ar/feed/politica'
+      },
+      {
+        id: 2,
+        title: 'Canal Policiales',
+        link: 'https://tn.com.ar/feed/policiales'
+      },
+      {
+        id: 3,
+        title: 'Canal Deportes',
+        link: 'https://tn.com.ar/feed/deportes'
+      },
+      {
+        id: 4,
+        title: 'Canal Internacional',
+        link: 'https://tn.com.ar/feed/internacional'
+      },
+      {
+        id: 5,
+        title: 'Canal Sociedad',
+        link: 'https://tn.com.ar/feed/sociedad'
+      },
+      {
+        id: 6,
+        title: 'Canal Tecno',
+        link: 'https://tn.com.ar/feed/tecnologia'
+      },
+      {
+        id: 7,
+        title: 'Canal Show',
+        link: 'https://tn.com.ar/feed/show'
+      },
+      {
+        id: 8,
+        title: 'La Viola',
+        link: 'https://tn.com.ar/feed/musica'
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: 'Perfil',
+    feeds: [
+      {
+        id: 0,
+        title: 'Ultimo Momento',
+        link: 'https://www.perfil.com/rss/ultimomomento.xml'
+      },
+      {
+        id: 1,
+        title: 'Politica',
+        link: 'https://www.perfil.com/rss/politica.xml'
+      },
+      {
+        id: 2,
+        title: 'Policia',
+        link: 'https://www.perfil.com/rss/policia.xml'
+      },
+      {
+        id: 3,
+        title: 'Internacionales',
+        link: 'https://www.perfil.com/rss/internacional.xml'
+      },
+      {
+        id: 4,
+        title: 'Economia',
+        link: 'https://www.perfil.com/rss/economia.xml'
+      },
+      {
+        id: 5,
+        title: 'Deportes',
+        link: 'https://www.perfil.com/rss/deportes.xml'
+      },
+      {
+        id: 6,
+        title: 'Opinion',
+        link: 'https://www.perfil.com/rss/columnistas.xml'
+      },
+      {
+        id: 7,
+        title: 'Sociedad',
+        link: 'https://www.perfil.com/rss/sociedad.xml'
+      },
+      {
+        id: 8,
+        title: 'Cultura',
+        link: 'https://www.perfil.com/rss/cultura.xml'
+      },
+      {
+        id: 9,
+        title: 'Espectaculos',
+        link: 'https://www.perfil.com/rss/espectaculos.xml'
+      },
+      {
+        id: 10,
+        title: 'Ciencia',
+        link: 'https://www.perfil.com/rss/ciencia.xml'
+      },
+      {
+        id: 11,
+        title: 'Salud',
+        link: 'https://www.perfil.com/rss/salud.xml'
+      },
+      {
+        id: 12,
+        title: 'Tecnologia',
+        link: 'https://www.perfil.com/rss/tecnologia.xml'
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: 'Cronista',
+    feeds: [
+      {
+        id: 0,
+        title: 'Cronista',
+        link: 'https://www.cronista.com/rss/feed.xml'
       }
     ]
   }
